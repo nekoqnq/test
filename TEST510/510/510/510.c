0ee771693126568a2cc8d2eb5d351821469d4914
nekoqnq <128674895+nekoqnq@users.noreply.github.com>
2024-05-10 21:10:21 +0800

    Add files via upload

---
+++ TEST510/510/510/510.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#define NUM 100//define定义表示符号
+//#define定义宏
+//#define ADD(x,y) ((x)+(y))
+//int main()
+//{
+//    //printf("%d\n", NUM);
+//    //int n = NUM;
+//    //printf("%d\n", n);
+//    //int arr[NUM] = { 0 };
+//    int a = 10;
+//    int b = 20;
+//    int c = ADD(a, b);
+//    printf("%d\n", c);
+//    return 0;
+//}
+
+ 
